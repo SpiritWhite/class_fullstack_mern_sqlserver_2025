1b466779f6c47268b5e58db8c90f5403181e5caa
Julio Perez <julioperezz@banpro.com.ni>
2025-10-10 23:11:54 -0600

    feature: class 12

---
+++ 2. React/class_12/src/App.jsx
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import { getPokemonList, getData } from './api/pokemon';
+import './assets/pokemon.css';
+import './assets/App.css';
+
+
+function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [paginate, setPaginate] = useState(0);
+  const [previousPag, setPreviousPag] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      try {
+        setLoading(true);
+        // Obtener los primeros 10 pokémones
+        const response = await getPokemonList(100,100 * paginate);
+        const pokemonData = response.data.results;
+
+        setPreviousPag(!response.data.previous)
+
+        // Obtener información detallada de cada pokémon
+        const pokemonDetails = await Promise.all(
+          pokemonData.map(async (pokemon) => {
+            const detailResponse = await getData(pokemon.url);
+            return detailResponse.data;
+          })
+        );
+
+        setPokemons(pokemonDetails);
+        setLoading(false);
+      } catch (err) {
+        setError('Error al cargar los pokémones');
+        setLoading(false);
+        console.error('Error:', err);
+      }
+    };
+
+    fetchPokemons();
+  }, [paginate]);
+
+  const onAddPag = () => setPaginate(prevState => prevState + 1);
+
+  const onRmvPag = () => setPaginate(prevState => prevState - 1);
+
+  const handlePokemon = (pokemonId) => (e) => navigate(`/pokemon/${pokemonId}`);
+
+  return (
+    <React.Fragment>
+      <div className="pokemon-list ">
+        <button onClick={onRmvPag} className="me-2" disabled={previousPag}>-1</button>
+        <button onClick={onAddPag}>+1</button>
+      <h2>Lista de Pokémones</h2>
+      <div className="pokemon-grid">
+        {
+        loading 
+        ? <h2>Cargando....</h2>
+        : pokemons.map((pokemon) => (
+          <div 
+          key={pokemon.id} 
+          className="pokemon-card pointer"
+          onClick={handlePokemon(pokemon.id)}
+          >
+            <img
+              src={pokemon.sprites.front_default}
+              alt={pokemon.name}
+              className="pokemon-image"
+            />
+            <h3 className="pokemon-name">
+              {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+            </h3>
+            <p className="pokemon-id">#{pokemon.id}</p>
+            <div className="pokemon-types">
+              {pokemon.types.map((typeInfo) => (
+                <span key={typeInfo.type.name} className={`type ${typeInfo.type.name}`}>
+                  {typeInfo.type.name}
+                </span>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+    </React.Fragment>
+  )
+}
+
+export default App
